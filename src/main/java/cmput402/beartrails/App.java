8389205768d7a41b805e7d5d5af57ddcec22939c
derek-repka <repka@ualberta.ca>
2019-02-15 21:15:51 -0700

    Initial Commit

---
+++ src/main/java/cmput402/beartrails/App.java
@@ -0,0 +1,13 @@
+package cmput402.beartrails;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
