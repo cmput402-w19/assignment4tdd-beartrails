f76fba0a7a3164b6e9aee1bf997d8d92501a4903
Corey Hunt <hunt1@ualberta.ca>
2019-02-20 10:20:19 -0700

    Added initializeDB method to ConnectionManager, and added comments

--- src/main/java/cmput402/beartrails/ConnectionManager.java
+++ src/main/java/cmput402/beartrails/ConnectionManager.java
@@ -7,29 +7,42 @@ public class ConnectionManager {
 	}
 	
 	/**
-	 * Open a connection to the database, 
-	 * using the default location
+	 * Initialize the database.  Will only proceed if the DB file
+	 * does not yet exist, otherwise will do nothing.
+	 * 
+	 * @return true if the database was created, false otherwise.
 	 */
-	public void openConnection() {
-		
+	private Boolean initializeDB() {
+		return null;
+	}
+	
+	/**
+	 * Open a connection to the database, 
+	 * using the default location. 
+	 * Create the database if it does not exist yet.
+	 * 
+	 */
+	public Boolean openConnection() {
+		return null;
 	}
 	
 	/**
 	 * Open a connection to the database 
-	 * located at dbPath
+	 * located at dbPath.
+	 * Create the database if it does not exist yet.
 	 * 
 	 * @param dbPath
 	 */
-	public void openConnection(String dbPath) {
-		
+	public Boolean openConnection(String dbPath) {
+		return null;
 	}
 	
 	/**
 	 * Close the connection with the database.
 	 * This is part of the proper cleanup process.
 	 */
-	public void closeConnection() {
-		
+	public Boolean closeConnection() {
+		return null;
 	}
 	
 	/**
