60f520d3b9500405195781a3f7affc95717907e6
Corey Hunt <hunt1@ualberta.ca>
2019-02-20 10:14:50 -0700

    Added skeleton for the ConnectionManager class

---
+++ src/main/java/cmput402/beartrails/ConnectionManager.java
@@ -0,0 +1,45 @@
+package cmput402.beartrails;
+
+public class ConnectionManager {
+	
+	public ConnectionManager() {
+		
+	}
+	
+	/**
+	 * Open a connection to the database, 
+	 * using the default location
+	 */
+	public void openConnection() {
+		
+	}
+	
+	/**
+	 * Open a connection to the database 
+	 * located at dbPath
+	 * 
+	 * @param dbPath
+	 */
+	public void openConnection(String dbPath) {
+		
+	}
+	
+	/**
+	 * Close the connection with the database.
+	 * This is part of the proper cleanup process.
+	 */
+	public void closeConnection() {
+		
+	}
+	
+	/**
+	 * Query the database.
+	 * 
+	 * @param sql the query to execute.
+	 * @return the result of the query.
+	 */
+	public Object query(String sql) {
+		return null;
+	}
+	
+}
