34393a91532a7f9dfd0cbda44916ea138b516124
Corey Hunt <hunt1@ualberta.ca>
2019-02-20 16:00:46 -0700

    RED added test for openConnection

---
+++ src/test/java/cmput402/beartrails/ConnectionManagerTest.java
@@ -0,0 +1,39 @@
+package cmput402.beartrails;
+
+import java.io.File;
+
+
+/*
+ * Note: purposefully NOT using mocks or stubs in this class,
+ *       as these tests become trivial if not actually accessing a DB on file.
+ *       That is, these are integration tests, not unit tests.
+ */
+
+import junit.framework.TestCase;
+
+public class ConnectionManagerTest extends TestCase {
+	
+	private final String dbTestFile = "db/test-temp.db";
+
+	public ConnectionManagerTest(String name) {
+		super(name);
+	}
+
+	/*
+	 * Simply tests that a database file is created if it didn't yet exist.
+	 * There doesn't appear to be a way to tell whether a file is currently open/in use.
+	 * Also, calling openConnection should initialize the database, but it's not possible
+	 * to test for that without calling query(), and that will be done in other tests.
+	 */
+	public void testOpenConnection() {
+		File db = new File(this.dbTestFile);
+		db.delete();
+		assertFalse(db.exists());
+		
+		ConnectionManager cm = new ConnectionManager();
+		cm.openConnection(this.dbTestFile);
+		assertTrue(db.exists());
+	
+	}
+
+}
