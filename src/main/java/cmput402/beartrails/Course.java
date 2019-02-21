da04d74d2fe2191c3099d380308eeeb5c02c6695
derek-repka <repka@ualberta.ca>
2019-02-21 16:06:44 -0700

    RED: Added test for getStudentGrades

--- src/main/java/cmput402/beartrails/Course.java
+++ src/main/java/cmput402/beartrails/Course.java
@@ -1,15 +1,13 @@
 package cmput402.beartrails;
 
-import java.sql.Time;
-
 public class Course {
     public enum DaysOfWeek{MonWedFri, TueThu}
 
-    public int courseId;
     public String courseSubject;
     public String courseNumber;
     public DaysOfWeek courseDays;
-    public Time startTime;
+    public Integer startTime;
     public Integer duration;
     public String location;
+    public String professor;
 }
--- src/test/java/cmput402/beartrails/GradeManagerTest.java
+++ src/test/java/cmput402/beartrails/GradeManagerTest.java
@@ -104,4 +104,71 @@ public class GradeManagerTest extends TestCase {
         Assert.assertEquals(result.get("molly"), 3.7d);
         Assert.assertEquals(result.get("james"), 2.0d);
     }
+
+    public void testgetStudentGrades()
+    {
+        ConnectionManager mockConnectionManager = mock(ConnectionManager.class);
+
+        List<List<Object>> fakeGrades = new ArrayList<List<Object>>();
+
+        List<Object> fakeCourseGrade = new ArrayList<Object>();
+        fakeCourseGrade.add("cmput");
+        fakeCourseGrade.add("402");
+        fakeCourseGrade.add(Course.DaysOfWeek.TueThu.name());
+        fakeCourseGrade.add(13);
+        fakeCourseGrade.add(1);
+        fakeCourseGrade.add("CAB");
+        fakeCourseGrade.add("snadi");
+        fakeCourseGrade.add(3.5d);
+        fakeGrades.add(fakeCourseGrade);
+
+        fakeCourseGrade = new ArrayList<Object>();
+        fakeCourseGrade.add("econ");
+        fakeCourseGrade.add("281");
+        fakeCourseGrade.add(Course.DaysOfWeek.TueThu.name());
+        fakeCourseGrade.add(11);
+        fakeCourseGrade.add(1);
+        fakeCourseGrade.add("T");
+        fakeCourseGrade.add("lpriem");
+        fakeCourseGrade.add(3.2d);
+
+        fakeGrades.add(fakeCourseGrade);
+
+        fakeCourseGrade = new ArrayList<Object>();
+        fakeCourseGrade.add("math");
+        fakeCourseGrade.add("222");
+        fakeCourseGrade.add(Course.DaysOfWeek.MonWedFri.name());
+        fakeCourseGrade.add(10);
+        fakeCourseGrade.add(1);
+        fakeCourseGrade.add("CAB");
+        fakeCourseGrade.add("tpas");
+        fakeCourseGrade.add(4.0d);
+        fakeGrades.add(fakeCourseGrade);
+
+        when(mockConnectionManager.query(anyString())).thenReturn(fakeGrades);
+
+        GradeManager gradeManager = new GradeManager(mockConnectionManager);
+        HashMap<Course, Double> result = gradeManager.getStudentGrades("repka");
+
+        assertNotNull(result);
+        assertTrue(result.size() == 3);
+        Course course = new Course();
+        course.courseSubject = "cmput";
+        course.courseNumber = "402";
+        course.courseDays = Course.DaysOfWeek.TueThu;
+        course.startTime = 13;
+        course.duration = 1;
+        course.professor = "snadi";
+        course.location = "CAB";
+
+        Course firstCourse = (Course)result.keySet().toArray()[0];
+
+        Assert.assertEquals(result.get(course), 3.7d);
+        Assert.assertTrue(firstCourse.courseSubject.equals(course.courseSubject));
+        Assert.assertTrue(firstCourse.courseDays.equals(course.courseDays));
+        Assert.assertTrue(firstCourse.startTime.equals(course.startTime));
+        Assert.assertTrue(firstCourse.location.equals(course.location));
+        Assert.assertTrue(firstCourse.professor.equals(course.professor));
+
+    }
 }
