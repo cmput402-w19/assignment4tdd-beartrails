716b4b9cdcc15035fe2d2996ec8c0951261d1e31
derek-repka <repka@ualberta.ca>
2019-02-21 20:50:17 -0700

    REFACTOR: Added sql query and parameter checks to createCourse

--- src/main/java/cmput402/beartrails/CourseManager.java
+++ src/main/java/cmput402/beartrails/CourseManager.java
@@ -13,15 +13,30 @@ public class CourseManager {
 
     public Boolean createCourse(Course course)
     {
-        String sub = course.courseSubject;
-        String num = course.courseNumber;
-        String loc = course.location;
-        String prof = course.professor;
-        String days = course.courseDays.name();
+        String sub = (course.courseSubject.isEmpty() || course.courseSubject == null)
+                     ? null
+                     : "\"" + course.courseSubject + "\"";
+        String num = (course.courseNumber.isEmpty() || course.courseNumber == null)
+                ? null
+                : "\"" + course.courseNumber + "\"";
+        String loc = (course.location.isEmpty() || course.location == null)
+                ? null
+                : "\"" + course.location + "\"";
+        String prof = (course.professor.isEmpty() || course.professor == null)
+                ? null
+                : "\"" + course.professor + "\"";
+        String days = (course.courseDays == null)
+                ? null
+                : "\"" + course.courseDays.name() + "\"";
         Integer start = course.startTime;
         Integer duration = course.duration;
 
-        return connectionManager.execute("");
+        String sqlQuery = "INSERT INTO courses VALUES (" + sub + ", " + num + ", " +
+                          days + ", " + start + ", " + duration + ", " + loc + ", " +
+                          prof + ");";
+        System.out.print(sqlQuery);
+
+        return connectionManager.execute(sqlQuery);
     }
 
     public List<User> getStudentsInCourse(String subject, String number)
