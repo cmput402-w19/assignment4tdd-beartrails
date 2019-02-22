5105a17e3702faaa142df2cdaa21f44296a52b9b
derek-repka <repka@ualberta.ca>
2019-02-21 21:08:45 -0700

    RED: Added test for getStudentsInCourse

--- src/test/java/cmput402/beartrails/CourseManagerTest.java
+++ src/test/java/cmput402/beartrails/CourseManagerTest.java
@@ -3,6 +3,9 @@ package cmput402.beartrails;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.times;
@@ -26,8 +29,51 @@ public class CourseManagerTest extends TestCase {
         newCourse.professor = "snadi";
 
         Boolean result = courseManager.createCourse(newCourse);
+        assertNotNull(result);
         Assert.assertTrue(result);
 
         verify(mockConnectionManager, times(1)).execute(anyString());
+    }
+
+    public void testgetStudentsInCourse()
+    {
+        ConnectionManager mockConnectionManager = mock(ConnectionManager.class);
+        List<List<Object>> fakeResponse = new ArrayList<List<Object>>();
+
+        List<Object> fakeStudents = new ArrayList<Object>();
+        fakeStudents.add("repka");
+        fakeStudents.add("derek");
+        fakeStudents.add("repka");
+        fakeStudents.add(2);
+        fakeResponse.add(fakeStudents);
+
+        fakeStudents = new ArrayList<Object>();
+        fakeStudents.add("hunt1");
+        fakeStudents.add("corey");
+        fakeStudents.add("hunt");
+        fakeStudents.add(2);
+        fakeResponse.add(fakeStudents);
+
+        fakeStudents = new ArrayList<Object>();
+        fakeStudents.add("araien");
+        fakeStudents.add("araien");
+        fakeStudents.add("redfern");
+        fakeStudents.add(2);
+        fakeResponse.add(fakeStudents);
+
+        when(mockConnectionManager.query(anyString())).thenReturn(fakeResponse);
+
+        CourseManager courseManager = new CourseManager(mockConnectionManager);
+        List<User> result = courseManager.getStudentsInCourse("CMPUT", "402");
+        assertNotNull(result);
+        Assert.assertTrue(result.size() == 3);
+        Assert.assertTrue(result.get(0).username.equals("repka"));
+        Assert.assertTrue(result.get(1).firstName.equals("corey"));
+        Assert.assertTrue(result.get(2).lastName.equals("redfern"));
+        Assert.assertTrue(result.get(0).userType.equals(User.Type.Student));
+        Assert.assertTrue(result.get(1).userType.equals(User.Type.Student));
+        Assert.assertTrue(result.get(2).userType.equals(User.Type.Student));
+
+        verify(mockConnectionManager, times(1)).query(anyString());
     }
 }
