42e07c1d9bc00de02341bc9b5ff4c184a35159f7
Z-Red <araien@ualberta.ca>
2019-02-22 10:41:07 -0700

    GREEN: testLogin

--- src/main/java/cmput402/beartrails/UserManager.java
+++ src/main/java/cmput402/beartrails/UserManager.java
@@ -2,6 +2,8 @@ package cmput402.beartrails;
 
 import java.util.List;
 
+import java.text.MessageFormat;
+
 public class UserManager {
 
     public ConnectionManager connectionManager;
@@ -11,9 +13,24 @@ public class UserManager {
         this.connectionManager = connectionManager;
     }
 
-    public User login(String username)
-    {
-        return null;
+    public User login(String username) {
+
+        Object[] params = new Object[]{username};
+        String query = MessageFormat.format("SELECT * FROM users WHERE user_name = {0} ORDER BY last_name;", params);
+        List<List<Object>> rs = connectionManager.query(query);
+
+        // Return null if there was no valid user
+        if (rs.size() == 0) {
+            return null;
+        }
+
+        // Extract attributes
+        String userName = (String) rs.get(0).get(0);
+        String firstName = (String) rs.get(0).get(1);
+        String lastName = (String) rs.get(0).get(2);
+        User.Type userType = (User.Type)rs.get(0).get(3);
+
+        return new User(userName, lastName, firstName, userType);
     }
 
     public boolean registerUser(User user)
