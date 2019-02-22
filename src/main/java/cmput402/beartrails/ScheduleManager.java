c5022aad0c93b53c6e4322d7aa178220a55d4658
Z-Red <araien@ualberta.ca>
2019-02-22 11:57:03 -0700

    GREEN: testGetStudentSchedule

--- src/main/java/cmput402/beartrails/ScheduleManager.java
+++ src/main/java/cmput402/beartrails/ScheduleManager.java
@@ -16,15 +16,29 @@ public class ScheduleManager {
         this.user = user;
     }
 
-    public List<Course> getStudentSchedule(String studentUsername)
-    {
-        return null;
+    public List<Course> getStudentSchedule(String studentUsername) {
+
+        Object[] params = new Object[]{studentUsername};
+        String query = MessageFormat.format("SELECT subject, number, days, time, duration, location" +
+                " FROM enrollments NATURAL JOIN courses ORDER BY days, time;" +
+                " WHERE student=\"{0}\";", params);
+        List<List<Object>> rs = connectionManager.query(query);
+
+        List<Course> courseList = new ArrayList<Course>();
+        for (List<Object> row : rs) {
+            String courseSubject = (String) row.get(0);
+            String courseNumber = (String) row.get(1);
+            Course.DaysOfWeek courseDays = (Course.DaysOfWeek) row.get(2);
+            Integer startTime = (Integer) row.get(3);
+            Integer duration = (Integer) row.get(4);
+            String location = (String) row.get(5);
+            courseList.add(new Course(courseSubject, courseNumber, courseDays, startTime, duration, location));
+        }
+
+        return courseList;
     }
 
-    public List<Course> getTeacherSchedule(String teacherUsername)
-    {
-        return null;
-    }
+    public List<Course> getTeacherSchedule(String teacherUsername) { return null; }
 
     public Boolean addCourse(String courseSubject, String courseNumber) {return null; }
 
