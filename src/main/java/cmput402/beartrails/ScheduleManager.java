3c623ae1fb9223dec6170a4d51962f4c9c994d46
Z-Red <araien@ualberta.ca>
2019-02-22 12:35:54 -0700

    GREEN: implement addCourse and getTeacherSchedule

--- src/main/java/cmput402/beartrails/ScheduleManager.java
+++ src/main/java/cmput402/beartrails/ScheduleManager.java
@@ -59,7 +59,42 @@ public class ScheduleManager {
         return courseList;
     }
 
-    public Boolean addCourse(String courseSubject, String courseNumber) {return null; }
+    public Boolean addCourse(String courseSubject, String courseNumber) {
+
+        Object[] selectParams = new Object[]{courseSubject, courseNumber};
+        String selectQuery = MessageFormat.format(
+                "SELECT days, time  FROM courses WHERE subject = {0} AND number = {1};", selectParams);
+        List<List<Object>> courseList = connectionManager.query(selectQuery);
+
+        // The course in question did not even exist
+        if (courseList.size() == 0) {
+            return false;
+        }
+
+        Course.DaysOfWeek day = (Course.DaysOfWeek) courseList.get(0).get(0);
+        Integer time = (Integer) courseList.get(0).get(1);
+
+        // Get the users current schedule
+        List<Course> userSchedule;
+        if (user.userType.equals(User.Type.Student)) {
+            userSchedule = getStudentSchedule(user.username);
+        } else {
+            userSchedule = getTeacherSchedule(user.username);
+        }
+
+        // If there is a conflict with the existing schedule, the new course cannot be added
+        for (Course course : userSchedule) {
+            if (course.courseDays.equals(day) && course.startTime.equals(time)) {
+                return false;
+            }
+        }
+
+        Object[] insertParams = new Object[]{user.username, courseSubject, courseNumber};
+        String insertQuery = MessageFormat.format(
+                "INSERT INTO enrollments VALUES (\"{0}\", \"{1}\", \"{2}\", null);", insertParams);
+
+        return(connectionManager.execute(insertQuery));
+    }
 
     public Boolean removeCourse(String courseSubject, String courseNumber)
     {
