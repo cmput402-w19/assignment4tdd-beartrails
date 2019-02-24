a23b3fc10e579426b2993456a034e692c9d13635
Corey Hunt <hunt1@ualberta.ca>
2019-02-24 14:48:36 -0700

    Test pass for ListCoursesMenuAction

--- src/main/java/cmput402/beartrails/ui/menu/ListCoursesMenuAction.java
+++ src/main/java/cmput402/beartrails/ui/menu/ListCoursesMenuAction.java
@@ -1,10 +1,42 @@
 package cmput402.beartrails.ui.menu;
 
+import java.util.List;
+
+import cmput402.beartrails.Course;
+
 public class ListCoursesMenuAction extends AbstractMenuAction {
 
 	@Override
 	public Boolean execute() {
-		return false;
+		if(this.courseManager == null) {
+			return false;
+		}
+		
+		List<Course> courseList = this.courseManager.getAllCourses();
+		
+		if(courseList.size() == 0) {
+			System.out.println("\nSorry, there are currently no courses entered into the system");
+		}
+		System.out.println();
+		//https://stackoverflow.com/a/18672745/2038127
+	    System.out.format("%15s%15s%15s%15s%15s%15s%15s\n", "Subject", "Number", 
+	    				  "Days", "Start Time", "Duration", 
+	    				  "Location", "Professor");
+		for(Course course: courseList) {
+			String subject = course.courseSubject;
+			String number = course.courseNumber.toString();
+			String days = course.courseDays.toString();
+			String startTime = course.startTime.toString() + ":00";
+			String duration = course.duration.toString();
+			String location = course.location;
+			String professor = course.professor;
+			
+			System.out.format("%15s%15s%15s%15s%15s%15s%15s\n", subject, number, 
+  				  days, startTime, duration, 
+  				  location, professor);
+		}
+		
+		return true;
 	}
 
 }
